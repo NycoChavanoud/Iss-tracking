1322bee9e62b0cc67b2e68ffc5ef0c8923cbbe51
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-12-23 20:45:13 +0100

    commit before merge

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </React.StrictMode>
